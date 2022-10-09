64db40fb9851490c9546c10242d7314d787fc9c3
pogoppo <5822076+pogoppo@users.noreply.github.com>
2022-10-09 12:53:06 +0900

    Svelte Kitの初期設定とデモアプリの整理、sassのインストール

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "./styles.css";
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style lang="scss">
+  main {
+    padding: 1rem;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  //
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>Home</h1>
+</section>
+
+<style lang="scss">
+  section {
+    h1 {
+      font-size: 2rem;
+    }
+  }
+</style>
